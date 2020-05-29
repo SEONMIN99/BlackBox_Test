5cab6727418014ee90851f3c086ce286a58464b2
SEONMIN99 <seonmin99@naver.com>
2020-05-29 11:18:13 +0900

    Create calculate method and Modify MyApp

--- MyApp.java
+++ MyApp.java
@@ -1,11 +1,15 @@
-
+import java.util.Scanner;
 /**
- * ABC수족관 입장료를 계산하는 메소드
+ * Main Method
  * 
- * @author (2018315034 정철우, 2018315038 이혜인, 2018315040 김선민 ) 
+ * @author (2018315034 정철우, 2018315038 이혜인, 2018315040 김선민) 
  * @version (2020.05.29)
  */
 public class MyApp
 {
-    
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("나이를 입력하세요: ");
+        Double age = sc.nextDouble();
+    }
 }
