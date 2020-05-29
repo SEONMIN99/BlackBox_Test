a86264f6434f072ca288f4522f7fb49ff29219a4
SEONMIN99 <seonmin99@naver.com>
2020-05-29 11:07:43 +0900

    Write author and version

--- MyApp.java
+++ MyApp.java
@@ -1,33 +1,11 @@
 
 /**
- * 여기에 MyApp 클래스 설명을 작성하십시오.
+ * ABC수족관 입장료를 계산하는 메소드
  * 
- * @author (작성자 이름) 
- * @version (버전번호나 날짜)
+ * @author (2018315034 정철우, 2018315038 이혜인, 2018315040 김선민 ) 
+ * @version (2020.05.29)
  */
 public class MyApp
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
-    private int x;
-
-    /**
-     * MyApp 클래스의 객체 생성자
-     */
-    public MyApp()
-    {
-        // 인스턴스 변수의 초기화
-        x = 0;
-    }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y   메소드의 예제 매개변수
-     * @return     x 더하기 y 
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하십시오.
-        return x + y;
-    }
+    
 }
